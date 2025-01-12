e67d7ca92da052889d29be5b82290e101b4121ee
Vikas Tiwari <vikastiwari809028@gmail.com>
2025-01-12 12:01:33 +0530

    Profile Update bug fixed

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://coursecrafterserver.onrender.com/api/v1";
-const BASE_URL = "http://localhost:5014/api/v1";
+const BASE_URL = "https://coursecrafterserver.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:5014/api/v1";
 
 const axiosInstance = axios.create();
 
