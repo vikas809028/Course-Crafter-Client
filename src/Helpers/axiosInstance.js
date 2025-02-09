ec7155937378619f2e1c4e8a3819f575691c3845
vikas809028 <vikastiwari809028@gmail.com>
2025-02-09 18:58:29 +0530

    Axios Updated

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-// const BASE_URL = "https://coursecrafterserver.onrender.com/api/v1";
-const BASE_URL = "http://localhost:5014/api/v1";
-
-const axiosInstance = axios.create();
-
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  withCredentials: true,
+});
 
 export default axiosInstance;
