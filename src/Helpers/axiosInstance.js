3efa9bd5dff13c3501658884661a87b886ea8091
vikas809028 <vikastiwari809028@gmail.com>
2025-02-09 19:31:01 +0530

    Axios Updated

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 const axiosInstance = axios.create({
-  baseURL: process.env.APP_BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
 export default axiosInstance;
+
